425539a7f49715816ac4faa571d48d1f2138007f
markrendle <mark@markrendle.net>
2010-12-15 18:39:36 +0000

    Added SL Test project

---
+++ TaskBoard.Tests/StoryViewModelTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TaskBoard.Data;
+using TaskBoard.Models;
+using TaskBoard.ViewModels;
+
+namespace TaskBoard.Tests
+{
+    [TestClass]
+    public class StoryViewModelTests
+    {
+        [TestMethod]
+        public void MovingAStoryFromBacklogToInProgressShouldSetItsStatusToInProgress()
+        {
+            var underTest = new MainViewModel(DesignTimeStories.Stories());
+            var story = underTest.Backlog.First();
+            underTest.Backlog.Remove(story);
+            underTest.InProgress.Add(story);
+            Assert.AreEqual(ItemStatus.InProgress, story.Status);
+        }
+    }
+}
